5fcb122299631e6fb9a2c58aca85c38031f28112
inderjeetsingh1704 <inderjeet.c7@gmail.com>
2024-05-10 15:35:50 +0530

    Update settings.py

--- django_azure_test/settings.py
+++ django_azure_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6h7i(_v(g62v%-bn_!np81&e_euum##yq69@2&wqm&8sx=d^b_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangoapptest.azurewebsites.net']
+ALLOWED_HOSTS = ['djangoapptestframework.azurewebsites.net']
 
 
 # Application definition
